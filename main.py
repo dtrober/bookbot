d316e21deba66e049a5e512ff32c32c5fff8dc84
dtrober <dylantrobertson1445@gmail.com>
2024-07-24 00:01:12 -0500

    Added char count

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ def main():
         file_contents = f.read()
     print(file_contents)
     print(count(file_contents))
+    print(char_count(file_contents))
 
 def count(string):
     words = string.split()
     return len(words)
+
+def char_count(string):
+    char_dict = {}
+    lower_string = string.lower()
+    for char in lower_string:
+        if not char in char_dict:
+            char_dict[char] = 1
+        else:
+            char_dict[char] +=1
+    return char_dict
+
 
 main()
